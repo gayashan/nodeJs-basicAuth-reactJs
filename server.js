748386826c353ab2dd96d748f1ecafbb686a1cd4
indika <indika.g@simulation.com.sg>
2020-03-25 19:41:30 +0800

    initial push

---
+++ server.js
@@ -0,0 +1,14 @@
+// const express = require('express')
+// const app = express
+
+const http = require('http')
+
+function onRequest(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    })
+    response.write('Hello World')
+    response.end()
+}
+
+http.createServer(onRequest).listen(8000)
